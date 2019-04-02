5503738c106fb176b62712dc148697aae9e6caa5
artwin22 <muhamad.14116050@student.itera.ac.id>
2019-04-02 19:48:56 +0700

    Menu

--- app/src/main/java/com/itera/itime/SplashScreenActivity.kt
+++ app/src/main/java/com/itera/itime/SplashScreenActivity.kt
@@ -19,7 +19,7 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        
+
         mDelayHandler = Handler()
 
         mDelayHandler!!.postDelayed(mRunnable,SPLASH_DELAY)

165f930f49f70216a7f270da7b0576906d957e06
artwin22 <muhamad.14116050@student.itera.ac.id>
2019-04-02 20:06:07 +0700

    SplashScreen Fix

--- app/src/main/java/com/itera/itime/SplashScreenActivity.kt
+++ app/src/main/java/com/itera/itime/SplashScreenActivity.kt
@@ -1,27 +1,24 @@
 package com.itera.itime
 
+import android.app.ActionBar
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
 
 class SplashScreenActivity : AppCompatActivity() {
-    private var mDelayHandler: Handler? = null
-    private var SPLASH_DELAY: Long = 3000
-
-    internal val mRunnable: Runnable = Runnable {
-        if(!isFinishing){
-            val intent = Intent(applicationContext,MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        super.onCreate(savedInstanceState)
+        window.requestFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash_screen)
 
-        mDelayHandler = Handler()
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashScreenActivity,MainActivity::class.java))
+            finish()
+        },4000)
 
-        mDelayHandler!!.postDelayed(mRunnable,SPLASH_DELAY)
-    }
+     }
 }

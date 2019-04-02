21639f38c9b688124a421e1e07b70e0b08cae864
artwin22 <muhamad.14116050@student.itera.ac.id>
2019-04-02 21:45:59 +0700

    Menu Nav Hamburger

--- app/src/main/java/com/itera/itime/MainActivity.kt
+++ app/src/main/java/com/itera/itime/MainActivity.kt
@@ -2,17 +2,25 @@ package com.itera.itime
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toolbar
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var drawer: DrawerLayout
+    private lateinit var toogle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val toolbar: Toolbar = findViewById(R.id.toolbar_main)
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->

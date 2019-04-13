8d6c11dc65701ebd10449b0c25b0eb402fd0e1c0
artwin22 <muhamad.14116050@student.itera.ac.id>
2019-04-13 16:02:48 +0700

    Google Maps Gradle

---
+++ app/src/main/java/com/itera/itime/Direction.kt
@@ -0,0 +1,12 @@
+package com.itera.itime
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Direction : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_direction)
+    }
+}

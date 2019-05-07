a5704516fbe7919a2bc4912bd2d78604ba392d13
taufiq2810 <taufiq28101998@gmail.com>
2019-05-08 00:07:15 +0700

    add class jadwal

---
+++ app/src/main/java/com/itera/itime/AddJadwalActivity.kt
@@ -0,0 +1,14 @@
+package com.itera.itime
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AddJadwalActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_jadwal)
+        
+
+    }
+}
---
+++ app/src/main/java/com/itera/itime/Jadwal.kt
@@ -0,0 +1,8 @@
+package com.itera.itime
+
+import java.time.Clock
+
+class Jadwal (var gedung:String, var namaRuangan:String, var namaMataKuliah:String, var sks:Int, var hari:String, var pukulAwal:String, var pukulAKhir:String, var Dosen:String){
+
+
+}
